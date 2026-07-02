935782c38f2944a4c405e4b2645b9b8edf9740d5
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-03 00:31:55 +0530

    IncrementOperators added

---
+++ IncrementOperators.java
@@ -0,0 +1,17 @@
+public class IncrementOperators {
+   public static void main(String args[]) {
+
+
+      // Initialize the variable
+      int num = 10;
+
+
+      // first print 10, then number is 
+      // increment to 11
+      System.out.println("Post increment = " + num++);
+
+
+      // num was 11, incremented to 12 and print
+      System.out.println("Pre increment = " + ++num);
+  }
+}
