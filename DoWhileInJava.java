1e95f85307fdaef040e695bd97b9d45720520186
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-06 00:15:08 +0530

    DoWhileInJava Added

---
+++ DoWhileInJava.java
@@ -0,0 +1,8 @@
+public class DoWhileInJava {
+    public static void main(String args[]){
+        do{
+            System.out.println("Do block");
+        }while(false);
+        System.out.println("Do executed at once");
+    }
+}
