8d447aa0da950ded1764ddd31c8546a49ccf5b8f
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-06 00:09:09 +0530

    WhileInJava added

---
+++ WhileInJava.java
@@ -0,0 +1,10 @@
+public class WhileInJava {
+    public static void main(String args[]){
+    int i =  5;
+    while(i < 10){
+        System.out.println(i + " ");
+        i++;
+    };
+    System.out.println("While Loop executed sucessfully!");
+    }
+}
