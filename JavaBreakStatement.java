d8f88eb9804fa24e6c010876d352543ba680d86c
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-05 23:20:01 +0530

    JavaBreakStatement added

---
+++ JavaBreakStatement.java
@@ -0,0 +1,10 @@
+public class JavaBreakStatement {
+ 	public static void main(String args[]) {
+     	for(int i = 1; i <= 10; i++) {
+         	// terminate the loop if i is 5.
+         	if(i == 5)
+            	break;
+         	System.out.print(i + " ");
+     	}
+  	}
+}
