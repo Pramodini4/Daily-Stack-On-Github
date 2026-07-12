fb4976dc3d1ae7e11c726367dcbfbf78d5864ed0
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-12 20:50:36 +0530

    TypeB function added

---
+++ Functions in java/TypeB.java
@@ -0,0 +1,12 @@
+public class TypeB {
+     public static int sumofTwoNumbers(){
+         int a = 10;
+         int b = 20;
+         int sum = a + b;
+         return sum;
+     }
+     public static void main(String args[]){
+        int sum = sumofTwoNumbers();
+        System.out.println(sum);
+     }
+}
