39a1f662e16fed03f8e98b7965843f013fe6a7c2
Pramodini Dhavane <pramodinipdhavane4@gmail.com>
2026-07-17 22:42:48 +0530

    FindLargestNo added

---
+++ Problems/FindLargestNo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FindLargestNo {
+    public static void main(String args[]){
+        int largest,arr[10],size;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array (1-10):");
+        size = sc.nextInt();
+        System.out.println("Enter array elements:");
+        for(int i = 0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+    }
+
+}
